e91538c7b43b25372d31aa04ff6bca50be0d4031
AmazingRecordingStudios <amazingrecordingstudios@gmail.com>
2020-08-13 02:14:54 +0200

    js: playing audio file specified in html element attribute

--- OEBPS/Misc/playback.js
+++ OEBPS/Misc/playback.js
@@ -1,4 +1,7 @@
+"use strict";
+
 var jsPlayingClass = "jsplaying";
+var fileNameAttribute = 'data-audio-file';
 var defaultAudio = new Audio('../Audio/odyssey6_01_homer.mp3');
 
 defaultAudio.onended = function() {
@@ -16,7 +19,7 @@ function playclip(element) {
 
 function togglePlayPauseAudio(element) {
 
-	var audio = getDefaultAudioForTesting(element);
+	var audio = getAudio(element);
 
  	//audio.play();
 
@@ -37,6 +40,19 @@ function isPausedOrStopped(element) {
 	var isPlaying = element.classList.contains(jsPlayingClass);
 
 	return  !(isPlaying); // || audio.paused
+}
+
+function getAudio(element) {
+	var audioDirPath = '../Audio/';
+
+	var audioFileName = element.getAttribute(fileNameAttribute);
+	var audio = new Audio(audioDirPath + audioFileName);
+
+	audio.onended = function() {
+  		element.classList.remove(jsPlayingClass);
+	};
+
+	return audio;
 }
 
 function getDefaultAudioForTesting(element) {
